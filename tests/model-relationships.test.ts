a6f315be518dc9fd3a39d0b3e42cc1cf9b0b67c2
Marshall Thompson <marshall@creativeideal.net>
2021-04-12 01:17:53 -0600

    Test model relationships

---
+++ tests/model-relationships.test.ts
@@ -0,0 +1,50 @@
+import { createPinia } from 'pinia'
+import { setup } from '../src/index'
+import { api } from './feathers'
+
+const pinia = createPinia()
+
+const { defineStore, BaseModel } = setup({ pinia, clients: { api } })
+
+class Message extends BaseModel {
+  text = ''
+  userId = null
+}
+const useMessagesService = defineStore({ servicePath: 'messages', Model: Message })
+const messagesService = useMessagesService()
+
+class User extends BaseModel {
+  id: number | string | void = undefined
+  name = ''
+  get messages() {
+    const messagesService = useMessagesService()
+    const messages = messagesService.findInStore({ query: { userId: this.id } }).data
+    return messages
+  }
+}
+const useUsersService = defineStore({ servicePath: 'users', Model: User })
+const usersService = useUsersService()
+
+let amogh: any
+
+const resetStore = () => {
+  api.service('messages').store = {}
+  api.service('users').store = {}
+}
+beforeAll(async () => {
+  resetStore()
+  amogh = await usersService.create({ name: 'Amogh Palnitkar' })
+
+  await Promise.all([
+    messagesService.create({ text: 'message 1', userId: amogh.id }),
+    messagesService.create({ text: 'message 2', userId: amogh.id }),
+    messagesService.create({ text: 'message 3', userId: amogh.id }),
+  ])
+})
+afterAll(() => resetStore())
+
+describe('Model Relationships', () => {
+  test('Access composed stores through es5 getters', async () => {
+    expect(amogh.messages.length).toBe(3)
+  })
+})
