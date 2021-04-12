fcfb4ec11062e468df9262fb46d6fd95ce55affe
Marshall Thompson <marshall@creativeideal.net>
2021-04-12 01:40:31 -0600

    Allow setting data through relationships

--- tests/model-relationships.test.ts
+++ tests/model-relationships.test.ts
@@ -22,6 +22,13 @@ class User extends BaseModel {
     const messages = messagesService.findInStore({ query: { userId: (this as any).id } }).data
     return messages
   }
+  set messages(messages) {
+    const messagesService = useMessagesService()
+    messages.forEach((message: any) => {
+      message.userId = (this as any).id
+      messagesService.addOrUpdate(message)
+    })
+  }
 }
 const useUsersService = defineStore({ servicePath: 'users', Model: User })
 const usersService = useUsersService()
@@ -47,5 +54,11 @@ afterAll(() => resetStore())
 describe('Model Relationships', () => {
   test('Access composed stores through es5 getters', async () => {
     expect(amogh.messages.length).toBe(3)
+  })
+
+  test('use the es5 setter to create new messages', () => {
+    expect(amogh.messages.length).toBe(3)
+    amogh.messages = [{ text: 'april showers bring may flowers' }]
+    expect(amogh.messages.length).toBe(4)
   })
 })
