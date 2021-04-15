b0e60ff51c6db96236c737ec84bb6699c065364c
Marshall Thompson <marshall@creativeideal.net>
2021-04-14 23:00:23 -0600

    remove unused reference of idField

--- src/service-store/base-model.ts
+++ src/service-store/base-model.ts
@@ -76,7 +76,7 @@ export class BaseModel {
    * clone the current record using the `createCopy` mutation
    */
   public clone(data: AnyData = {}): this {
-    const { idField, store } = this.constructor as typeof BaseModel
+    const { store } = this.constructor as typeof BaseModel
     return (store as any).clone(this, data)
   }
 
