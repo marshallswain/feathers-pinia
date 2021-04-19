27eaf99a5e7a9ce09dcfdb9db9d349d7a6bd292b
Marshall Thompson <marshall@creativeideal.net>
2021-04-18 19:47:51 -0600

    feat: pass compress: false to disable storage compression

--- src/storage-sync.ts
+++ src/storage-sync.ts
@@ -4,16 +4,16 @@ import { computed, watch } from 'vue'
 import lz from 'lz-string'
 
 // Writes data to localStorage
-export function writeToStorage(id: string, data: any, storage: any) {
-  const compressed = lz.compress(JSON.stringify(data))
-  storage.setItem(id, compressed)
+export function writeToStorage(id: string, data: any, storage: any, { compress = true }) {
+  const toWrite = compress ? lz.compress(JSON.stringify(data)) : JSON.stringify(data)
+  storage.setItem(id, toWrite)
 }
 
 // Reads and decompresses data from localStorage
-export function hydrateStore(store: any, storage: any) {
+export function hydrateStore(store: any, storage: any, { compress = true }) {
   const data = storage.getItem(store.$id)
   if (data) {
-    const hydrationData = JSON.parse(lz.decompress(data) as string)
+    const hydrationData = JSON.parse(compress ? (lz.decompress(data) as string) : data)
     Object.keys(hydrationData).forEach((key: string) => {
       store[key] = hydrationData[key]
     })
@@ -28,9 +28,10 @@ export function hydrateStore(store: any, storage: any) {
 export function syncWithStorage(
   store: any,
   stateKeys: Array<string> = [],
-  storage: Storage = window.localStorage
+  storage: Storage = window.localStorage,
+  options: { compress: boolean } = { compress: true }
 ) {
-  hydrateStore(store, storage)
+  hydrateStore(store, storage, options)
 
   const debouncedWrite = debounce(writeToStorage, 500)
   const toWatch = computed(() => _.pick(store, ...stateKeys))
@@ -38,7 +39,7 @@ export function syncWithStorage(
   watch(
     () => toWatch.value,
     (val) => {
-      debouncedWrite(store.$id, val, storage)
+      debouncedWrite(store.$id, val, storage, options)
     },
     { deep: true }
   )
