4b9409733026a9461c8cb2b56d41516a391d7e3a
Eshan Joshi <eshan@mavericklabs.net>
2021-05-04 17:40:28 +0530

    A question for Marshall

--- src/clear-storage.ts
+++ src/clear-storage.ts
@@ -1,4 +1,6 @@
 export function clearStorage(storage: Storage = window.localStorage) {
+    //  Question @Marshall 
+    //  For mocked storage how can we iterate over keys?
     Object.keys(storage).map(key => {
         if (key.startsWith("service.")) {
             storage.removeItem(key)
