c9726d8ab2fe37f6fe82164dd2c506a186ecedfc
Ray Foss <ray@vblip.com>
2021-09-21 14:47:08 -0500

    set hmr client port to auto configure in cs

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    hmr: {
+      port: parseInt(process.env.KUBERNETES_SERVICE_PORT, 10) || 3000
+    }
+  },
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
