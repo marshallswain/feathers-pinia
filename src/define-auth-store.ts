7fedb79a5f4a9595aa1eed23340339e7545b93f8
fratzinger <22286818+fratzinger@users.noreply.github.com>
2022-01-26 00:19:40 +0100

    feat: authStore fully typed

--- src/define-auth-store.ts
+++ src/define-auth-store.ts
@@ -1,27 +1,75 @@
-import { defineStore } from 'pinia'
+import { defineStore, DefineStoreOptionsBase, PiniaCustomProperties, StateTree, Store, StoreDefinition, _GettersTree, _StoreWithGetters, _StoreWithState } from 'pinia'
+import { UnwrapRef } from "vue-demi";
 
-interface SetupAuthOptions {
+interface DefineAuthStoreOptions<
+  Id extends string,
+  S extends StateTree,
+  G /* extends GettersTree<S> */,
+  A /* extends Record<string, StoreAction> */
+> extends DefineStoreOptionsBase<S, Store<Id, S, G, A>> {
   feathersClient: any
-  id?: string
-  state?: Function
-  getters?: { [k: string]: any }
-  actions?: { [k: string]: any }
+  id?: Id
+  state?: () => S
+
+  getters?: G &
+     ThisType<UnwrapRef<AS<S>> & _StoreWithGetters<AS<S>> & PiniaCustomProperties> &
+     _GettersTree<S>
+ 
+   actions?: A &
+     ThisType<
+       A &
+         UnwrapRef<AS<S>> &
+         _StoreWithState<Id, AS<S>, AG<G>, AA<A>> &
+         _StoreWithGetters<AG<G>> &
+         PiniaCustomProperties
+     >
 }
 
-export function defineAuthStore(...args: [SetupAuthOptions] | [string, SetupAuthOptions]): any {
+interface AuthStoreDefaultState {
+  isLoading: boolean,
+  isAuthenticated: boolean,
+  accessToken: string | null, // The auth0 and API accessToken
+  payload: any, // accessToken payload
+  error: any,
+}
+
+interface AuthStoreDefaultGetters extends _GettersTree<AuthStoreDefaultState> {
+  feathersClient: () => any
+}
+
+interface AuthStoreDefaultActions {
+  authenticate: (authData: any) => Promise<any>
+  handleResponse: (response: any) => any
+  handleError: (err: Error) => any
+  setLoaded: (val: boolean) => void
+}
+
+type AS<S> = AuthStoreDefaultState & S;
+type AG<G> = AuthStoreDefaultGetters & G
+type AA<A> = AuthStoreDefaultActions & A;
+
+export function defineAuthStore<
+  Id extends string,
+  S extends StateTree = {},
+  G extends _GettersTree<S> = {},
+  // cannot extends ActionsTree because we loose the typings
+  A /* extends ActionsTree */ = {}
+>(
+  ...args: [DefineAuthStoreOptions<Id, S, G, A>] | [Id, Omit<DefineAuthStoreOptions<Id, S, G, A>, 'id'>]
+): StoreDefinition<Id, AS<S>, AG<G>, AA<A>> {
   const id = args.length === 2 ? args[0] : args[0].id || 'auth'
   const options = args.length === 2 ? args[1] : args[0]
   const {
     feathersClient,
-    state = () => ({}),
-    getters = {},
-    actions = {},
+    state = () => ({}) as S,
+    getters = {} as G,
+    actions = {} as A,
   } = options
 
   /**
    * Default State
    */
-  const defaultState = {
+  const defaultState: AuthStoreDefaultState = {
     isLoading: true,
     isAuthenticated: false,
     accessToken: null, // The auth0 and API accessToken
@@ -29,7 +77,10 @@ export function defineAuthStore(...args: [SetupAuthOptions] | [string, SetupAuth
     error: null,
   }
 
-  const defaultGetters = {
+  /**
+   * Default Getters
+   */
+  const defaultGetters: AuthStoreDefaultGetters = {
     feathersClient() {
       return feathersClient
     },
@@ -38,7 +89,7 @@ export function defineAuthStore(...args: [SetupAuthOptions] | [string, SetupAuth
   /**
    * Default Actions
    */
-  const defaultActions = {
+  const defaultActions: AuthStoreDefaultActions = {
     async authenticate(authData: any) {
       try {
         const response = await feathersClient.authenticate(authData)
@@ -68,16 +119,16 @@ export function defineAuthStore(...args: [SetupAuthOptions] | [string, SetupAuth
      * For tracking first-load state. Used by the watcher, below.
      */
     setLoaded() {
-      // eslint-disable-next-line @typescript-eslint/no-extra-semi
-      ;(this as any).isLoading = false
+      (this as any).isLoading = false
     },
   }
 
   const useAuth = defineStore({
-    id,
+    id: id as Id,
     state: () => Object.assign(defaultState, state()),
     getters: Object.assign(defaultGetters, getters),
     actions: Object.assign(defaultActions, actions),
   })
+
   return useAuth
 }
