a1abd4e61af6302e4576829006a76dda51ac0072
Marshall Thompson <marshall@creativeideal.net>
2022-09-11 12:38:53 -0600

    refactor: usePagination on usePageData

--- src/use-pagination.ts
+++ src/use-pagination.ts
@@ -1,10 +1,35 @@
+import type { MaybeRef } from './utility-types'
 import { computed, watch, Ref, unref } from 'vue-demi'
+import { usePageData } from './utils-pagination'
 
-export function usePagination(pagination: any, latestQuery: Ref) {
+interface Pagination {
+  $limit: number
+  $skip: number
+}
+
+export function usePagination(pagination: MaybeRef<Pagination>, latestQuery: Ref) {
+  const set = (pagination: any, key: string, val: number) => {
+    (pagination.value || pagination)[key] = val
+  }
+  const $limit = computed({
+    set: (val) => set(pagination, '$limit', val),
+    get: () => unref(pagination).$limit,
+  })
+  const $skip = computed({
+    set: (val) => set(pagination, '$skip', val),
+    get: () => unref(pagination).$skip,
+  })
+  const total = computed(() => latestQuery.value.response.total)
+  const { pageCount, currentPage, canPrev, canNext, toStart, toEnd, toPage, next, prev } = usePageData(
+    $limit,
+    $skip,
+    total,
+  )
+
   /**
    * The number of items returned in the latestQuery prop.
    */
-   const itemsCount = computed(() => {
+  const itemsCount = computed(() => {
     const q = unref(latestQuery)
     if (q && q.response) {
       return q.response.total
@@ -13,50 +38,7 @@ export function usePagination(pagination: any, latestQuery: Ref) {
     }
   })
 
-  /**
-   * The number of pages available based on the results returned in the latestQuery prop.
-   */
-  const pageCount = computed(() => {
-    const q = unref(latestQuery)
-    const { $limit } = unref(pagination)
-    if (q && q.response) {
-      return Math.ceil(q.response.total / $limit)
-    } else {
-      return 1
-    }
-  })
-
-  /**
-   * The `currentPage` is calculated based on the $limit and $skip values
-   */
-  const currentPage = computed({
-    set(pageNumber: number) {
-      if (pageNumber < 1) {
-        pageNumber = 1
-      } else if (pageNumber > pageCount.value) {
-        pageNumber = pageCount.value
-      }
-      const { $limit } = unref(pagination)
-      const $skip = $limit * (pageNumber - 1)
-
-      const newPagination = { $limit, $skip }
-
-      if (pagination.value) {
-        pagination.value = newPagination
-      } else {
-        Object.assign(pagination, newPagination)
-      }
-    },
-    get() {
-      const params = unref(pagination)
-      if (params) {
-        return pageCount.value === 0 ? 0 : params.$skip / params.$limit + 1
-      } else {
-        return 1
-      }
-    },
-  })
-
+  // If the max page becomes lower, go to the last page to still show data
   watch(
     () => pageCount.value,
     () => {
@@ -66,30 +48,6 @@ export function usePagination(pagination: any, latestQuery: Ref) {
       }
     },
   )
-
-  const canPrev = computed(() => {
-    return currentPage.value - 1 > 0
-  })
-  const canNext = computed(() => {
-    return currentPage.value < pageCount.value
-  })
-
-  function toStart(): void {
-    currentPage.value = 1
-  }
-  function toEnd(): void {
-    currentPage.value = pageCount.value
-  }
-  function toPage(pageNumber: number): void {
-    currentPage.value = pageNumber
-  }
-
-  function next(): void {
-    currentPage.value++
-  }
-  function prev(): void {
-    currentPage.value--
-  }
 
   return {
     itemsCount,
