e8dcc7942ea5a0a72da27e8aa9afb6bb72f806e5
Marshall Thompson <marshall@creativeideal.net>
2022-12-07 22:13:23 -0700

    refactor: cleaner useModelInstance

--- src/use-base-model/use-model-instance.ts
+++ src/use-base-model/use-model-instance.ts
@@ -1,4 +1,4 @@
-import type { BaseModelProps } from './types'
+import type { BaseModelProps, WithModel } from './types'
 import { reactive } from 'vue'
 import { AnyData } from '../use-service'
 import ObjectID from 'isomorphic-mongo-objectid'
@@ -13,12 +13,13 @@ interface UseBaseModelOptions<TempId extends string = '__tempId'> {
 export const useInstanceModel = <M extends AnyData, TempId extends string = '__tempId'>(
   data: M,
   options: UseBaseModelOptions<TempId>,
-): M & BaseModelProps<M, TempId> => {
+) => {
   const { name, idField, tempIdField = '__tempId' } = options
   const __isClone = data.__isClone || false
 
-  const _data = data as M & BaseModelProps<M, TempId>
-  defineBaseModelProps({
+  const _data = data as M & WithModel<M>
+
+  const asBaseModel = defineBaseModelProps({
     data: _data,
     name,
     isClone: __isClone,
@@ -26,51 +27,61 @@ export const useInstanceModel = <M extends AnyData, TempId extends string = '__t
     tempIdField,
   })
 
-  const newData = reactive(_data)
+  const newData = reactive(asBaseModel) as typeof asBaseModel
   return newData
 }
 
 interface DefineBaseModelPropsOptions<M extends AnyData> {
-  data: M
+  data: M & WithModel<M>
   name: string
   isClone: boolean
   idField: string
   tempIdField: string
 }
 
-export const defineBaseModelProps = <M extends AnyData>({
+export const defineBaseModelProps = <M extends AnyData, TempId extends string>({
   data,
   name,
   isClone,
   idField,
   tempIdField,
 }: DefineBaseModelPropsOptions<M>) => {
-  Object.defineProperties(data, {
-    __modelName: {
+  const cloneMethods = {
+    clone(this: M) {
+      return this
+    },
+    commit(this: M) {
+      return this
+    },
+    reset(this: M) {
+      return this
+    },
+  }
+
+  defineProperties(data, {
+    __modelName: name,
+    __isClone: isClone,
+    __idField: idField,
+    __tempIdField: tempIdField,
+    [tempIdField]: data[idField] == null ? new ObjectID().toString() : undefined,
+    clone: cloneMethods.clone,
+    commit: cloneMethods.commit,
+    reset: cloneMethods.reset,
+  })
+
+  return data as M & BaseModelProps<M, TempId>
+}
+
+/**
+ * Defines all provided properties as non-enumerable and configurable
+ */
+const defineProperties = <M extends AnyData, D extends AnyData>(data: M, properties: D) => {
+  Object.keys(properties).forEach((key) => {
+    Object.defineProperty(data, key, {
       enumerable: false,
       configurable: true,
-      value: name,
-    },
-    __isClone: {
-      enumerable: false,
-      configurable: true,
-      value: isClone,
-    },
-    __idField: {
-      enumerable: false,
-      configurable: true,
-      value: idField,
-    },
-    __tempIdField: {
-      enumerable: false,
-      configurable: true,
-      value: tempIdField,
-    },
-    [tempIdField]: {
-      enumerable: false,
-      configurable: true,
-      value: data[idField] == null ? new ObjectID().toString() : undefined,
-    },
+      value: properties[key],
+    })
   })
   return data
 }
