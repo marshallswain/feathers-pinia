d85eae477046514a06c0f0d77fa9651373ddf24b
Marshall Thompson <marshall@creativeideal.net>
2022-12-23 10:26:32 -0700

    fix: hook pending state

--- src/hooks/hook-set-pending.ts
+++ src/hooks/hook-set-pending.ts
@@ -5,14 +5,20 @@ export const setPending = (Model: any) => async (context: HookContext, next: Nex
   const method = context.method === 'find' ? (context.params.query?.$limit === 0 ? 'count' : 'find') : context.method
 
   store.setPending(method, true)
-  if (context.id && method !== 'get') {
+  if (context.id != null && method !== 'get') {
     store.setPendingById(context.id, method, true)
+  }
+  const isTemp = context.data?.__isTemp
+  const tempId = context.data?.__tempId
+  if (isTemp && method === 'create') {
+    store.setPendingById(context.data.__tempId, method, true)
   }
 
   const unsetPending = () => {
     store.setPending(method, false)
-    if (context.id && method !== 'get') {
-      store.setPendingById(context.id, method, false)
+    const id = context.id != null ? context.id : tempId
+    if (id != null && method !== 'get') {
+      store.setPendingById(id, method, false)
     }
   }
 
