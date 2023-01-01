cfc5d7f3b13dc6b7164d78c2c668e778d79f9c76
Marshall Thompson <marshall@creativeideal.net>
2023-01-01 16:53:18 -0700

    chore: nuxt module code moved to its own repo
    https://github.com/marshallswain/nuxt-feathers-pinia

--- src/nuxt-module/runtime/plugin.ts
+++
@@ -1,7 +0,0 @@
-import type { FP } from '../types'
-import { defineNuxtPlugin } from '#app'
-
-export default defineNuxtPlugin(() => {
-  const fp: FP = { models: {} }
-  return { provide: { fp } }
-})
--- src/nuxt-module/types.ts
+++
@@ -1,9 +0,0 @@
-export interface ModelNamespace {
-  model: any
-  store: any
-  ready: boolean
-  final: boolean
-}
-export interface FP {
-  models: Record<string, ModelNamespace>
-}
