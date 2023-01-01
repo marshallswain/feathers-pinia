f683fbbb5d545a114c848c1752cb20a57e6eb225
Marshall Thompson <marshall@creativeideal.net>
2023-01-01 16:28:36 -0700

    chore: fix type error in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [vue(), dts()],
   server: {
     hmr: {
-      port: parseInt(process.env.KUBERNETES_SERVICE_PORT, 10) || 3000,
+      port: parseInt(process.env.KUBERNETES_SERVICE_PORT as string, 10) || 3000,
     },
   },
   build: {
