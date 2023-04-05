47d794eb3e3ccb68d970d55487343002cb4131b3
Marshall Thompson <marshall@creativeideal.net>
2023-04-04 18:59:44 -0600

    fix: refactor & test event system

--- src/use-data-store/events.ts
+++ src/use-data-store/events.ts
@@ -2,20 +2,15 @@ import type { FeathersService } from '@feathersjs/feathers'
 import type { HandleEvents, HandledEvents } from './types'
 import type { AnyData } from '../types'
 import { del, ref, set } from 'vue-demi'
-import { getId, hasOwn } from '../utils'
+import { convertData, getId, hasOwn } from '../utils'
 import _debounce from 'just-debounce'
+import type { PiniaService } from '../create-pinia-service'
 
 interface UseServiceStoreEventsOptions<M extends AnyData> {
-  service: FeathersService
-  getModel: () => (data: AnyData) => any
-  idField: string
+  service: PiniaService<FeathersService>
   debounceEventsTime?: number
-  onAddOrUpdate: (item: any) => void
-  onRemove: (item: any) => void
   debounceEventsGuarantee?: boolean
   handleEvents?: HandleEvents<M>
-  toggleEventLock: any
-  eventLocks: any
 }
 
 export const useServiceEvents = <M extends AnyData>(options: UseServiceStoreEventsOptions<M>) => {
@@ -30,7 +25,7 @@ export const useServiceEvents = <M extends AnyData>(options: UseServiceStoreEven
     async () => {
       const values = Object.values(addOrUpdateById.value)
       if (values.length === 0) return
-      options.onAddOrUpdate(values)
+      service.store.addToStore(values)
       addOrUpdateById.value = {}
     },
     options.debounceEventsTime || 20,
@@ -39,7 +34,7 @@ export const useServiceEvents = <M extends AnyData>(options: UseServiceStoreEven
   )
 
   function enqueueAddOrUpdate(item: any) {
-    const id = getId(item, options.idField)
+    const id = getId(item, service.store.idField)
     if (!id) return
 
     set(addOrUpdateById, id, item)
@@ -53,7 +48,7 @@ export const useServiceEvents = <M extends AnyData>(options: UseServiceStoreEven
     () => {
       const values = Object.values(removeItemsById.value)
       if (values.length === 0) return
-      options.onRemove(values)
+      service.store.removeFromStore(values)
       removeItemsById.value = {}
     },
     options.debounceEventsTime || 20,
@@ -62,7 +57,7 @@ export const useServiceEvents = <M extends AnyData>(options: UseServiceStoreEven
   )
 
   function enqueueRemoval(item: any) {
-    const id = getId(item, options.idField)
+    const id = getId(item, service.store.idField)
     if (!id) return
 
     set(removeItemsById, id, item)
@@ -80,49 +75,37 @@ export const useServiceEvents = <M extends AnyData>(options: UseServiceStoreEven
      * For `created` events, we don't know the id since it gets assigned on the server. Also, since `created` events
      * arrive before the `create` response, we only act on other events. For all other events, toggle the event lock.
      */
-    const id = getId(item, options.idField)
-    if (eventName !== 'created' && options.eventLocks[eventName][id]) {
-      options.toggleEventLock(id, eventName)
+    const id = getId(item, service.store.idField)
+    if (eventName !== 'created' && service.store.eventLocks[eventName][id]) {
+      service.store.toggleEventLock(id, eventName)
       return
     }
 
     if (handler) {
-      const Model = options.getModel()
-      const handled = handler(item, { model: Model })
+      const handled = handler(item, { service })
       if (!handled) return
     }
 
-    if (!options.debounceEventsTime) eventName === 'removed' ? options.onRemove(item) : options.onAddOrUpdate(item)
+    if (!options.debounceEventsTime)
+      eventName === 'removed' ? service.store.removeFromStore(item) : service.store.addToStore(item)
     else eventName === 'removed' ? enqueueRemoval(item) : enqueueAddOrUpdate(item)
   }
 
   // Listen to socket events when available.
   service.on('created', (item: any) => {
-    // const Model = options.getModel() as Func & EventEmitter
-    // const instance = Model(item)
-    // handleEvent('created', instance)
-    handleEvent('created', item)
-    // Model?.emit && Model.emit('created', instance)
+    const data = convertData(service, item)
+    handleEvent('created', data)
   })
   service.on('updated', (item: any) => {
-    // const Model = options.getModel() as Func & EventEmitter
-    // const instance = Model(item)
-    // handleEvent('updated', instance)
-    handleEvent('updated', item)
-    // Model?.emit && Model.emit('updated', instance)
+    const data = convertData(service, item)
+    handleEvent('updated', data)
   })
   service.on('patched', (item: any) => {
-    // const Model = options.getModel() as Func & EventEmitter
-    // const instance = Model(item)
-    // handleEvent('patched', instance)
-    handleEvent('patched', item)
-    // Model?.emit && Model.emit('patched', instance)
+    const data = convertData(service, item)
+    handleEvent('patched', data)
   })
   service.on('removed', (item: any) => {
-    // const Model = options.getModel() as Func & EventEmitter
-    // const instance = Model(item)
-    // handleEvent('removed', instance)
-    handleEvent('removed', item)
-    // Model?.emit && Model.emit('removed', instance)
+    const data = convertData(service, item)
+    handleEvent('removed', data)
   })
 }
