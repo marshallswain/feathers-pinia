72003264ad5e4085172b0fdb9397e696c7f07b58
Marshall Thompson <marshall@creativeideal.net>
2023-04-28 13:35:21 -0600

    fix clearStorage prefix

--- src/localstorage/clear-storage.ts
+++ src/localstorage/clear-storage.ts
@@ -6,7 +6,7 @@
  */
 export function clearStorage(storage: Storage = window.localStorage) {
   Object.keys(storage).map((key) => {
-    if (key.startsWith('service.')) {
+    if (key.startsWith('service:')) {
       storage.removeItem(key)
     }
   })
