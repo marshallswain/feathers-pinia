3577275bf334acb5f4b893d87ad8bd922fcd6224
Javad Mnjd <javad.mnjd@hotmail.com>
2024-05-24 05:05:14 +0330

    fix: update the unpaginated response handling

--- src/hooks/6-normalize-find.ts
+++ src/hooks/6-normalize-find.ts
@@ -20,21 +20,14 @@ export function normalizeFind() {
 
     next && await next()
 
-    if (context.method === 'find' && !context.result?.data) {
-      if (Array.isArray(context.result)) {
-        context.result = {
-          data: context.result,
-          limit: context.result.length,
-          skip: 0,
-          total: context.result.length,
-        }
-      } else {
-        context.result = {
-          data: [],
-          limit: context.params.$limit,
-          skip: context.params.$skip,
-          total: 0,
-        }
+    // this makes sure it only affects finds that are not paginated and are not custom.
+    // so the custom find responses fall through.
+    if (context.method === 'find' && !context.result?.data && Array.isArray(context.result)) {
+      context.result = {
+        data: context.result,
+        limit: context.params.$limit || context.result.length,
+        skip: context.params.$skip || 0,
+        total: context.result.length,
       }
     }
   }
